fa19434f1f8d384a74a3b1ded2ba76f1d3e0ec62
Noah Berns <nb_ufp@outlook.com>
2025-01-21 17:34:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-//The following space is for interactivity, Java script is used to make things within a site like search functions, chat rooms, or anything outside of the scope of Design and Structure of a site//
+//The following space is for interactivity, Java script is used to make things within a site like search functions, //
+// chat rooms, or anything outside of the scope of Design and Structure of a site//
